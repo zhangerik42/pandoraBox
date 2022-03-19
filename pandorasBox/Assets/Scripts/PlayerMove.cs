552f5a215bea39ff2d0c9b6828a0cb4adb0a5400
compsci4life480 <eriko480@Gmail.com>
2022-03-18 23:51:55 -0600

    move player

--- pandorasBox/Assets/Scripts/PlayerMove.cs
+++ pandorasBox/Assets/Scripts/PlayerMove.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerMove : MonoBehaviour
 {
     public float speed = 100;
     public Rigidbody2D rb;
+    public SpriteRenderer sr;
 
     // Update is called once per frame
     void Update()
@@ -13,8 +15,23 @@ public class PlayerMove : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        Vector3 tempVect = new Vector3(h, v, 0);
-        tempVect = tempVect.normalized * speed * Time.deltaTime;
-        rb.MovePosition(rb.transform.position + tempVect);
+        // if moving left, flip character
+        while(h < 0)
+        {
+            Debug.Log("flip");
+            sr.flipX = true;
+        }
+
+        if (h == 0 && v == 0)
+        {
+
+        }
+        else
+        {
+            Vector3 tempVect = new Vector3(h, v, 0);
+            tempVect = tempVect.normalized * speed * Time.deltaTime;
+            Debug.Log(tempVect);
+            rb.MovePosition(rb.transform.position + tempVect);
+        }
     }
 }
