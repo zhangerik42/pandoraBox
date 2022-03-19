bc13d473db42da1ff072d4ebe6bab085547503ea
kutayeken55 <kutayeken1@gmail.com>
2022-03-18 22:37:11 -0600

    Test PlayerMove

---
+++ pandorasBox/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TEST
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
