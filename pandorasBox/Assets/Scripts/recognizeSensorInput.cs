11bcac61892dd82fdda7cd109c2cae7127b5ffca
compsci4life480 <101242034+zhangerik42@users.noreply.github.com>
2022-03-19 01:51:56 -0600

    more perks + starting sensor code

---
+++ pandorasBox/Assets/Scripts/recognizeSensorInput.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recognizeSensorInput : MonoBehaviour
+{
+    public bool lightActive;
+    public GameObject light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.SetActive(lightActive);
+    }
+}
