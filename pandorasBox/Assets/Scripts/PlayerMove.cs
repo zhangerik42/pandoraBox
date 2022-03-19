dc27008e6799940f27da67c8a8b55ddecf82e225
compsci4life480 <eriko480@Gmail.com>
2022-03-18 23:12:15 -0600

    redownload 2d files

--- pandorasBox/Assets/Scripts/PlayerMove.cs
+++ pandorasBox/Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        //TEST
-    }
+    public float speed = 100;
+    public Rigidbody2D rb;
 
     // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 tempVect = new Vector3(h, v, 0);
+        tempVect = tempVect.normalized * speed * Time.deltaTime;
+        rb.MovePosition(rb.transform.position + tempVect);
     }
 }
