f78ab3dabed8f6d488d12747e775573883fd3eb0
kutayeken55 <99044189+kutayeken55@users.noreply.github.com>
2022-03-19 03:03:40 -0600

    fixed bugs

---
+++ powerUpCollision.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision){
+        //Detsroys object that collides with
+        //the obt that this script is attached tojec
+        Destroy(collision.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
